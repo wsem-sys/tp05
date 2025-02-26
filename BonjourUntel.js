5029b7ee2f7fc583cf6a36d038056ec673315732
wsem-sys <wsem@outlook.fr>
2025-02-26 16:58:09 +0100

    Initial

---
+++ BonjourUntel.js
@@ -0,0 +1,9 @@
+// fais par moi wsem celui qui marquera l histoire d'un W 
+// Je declare une fonction qui prend un nom en paramètre et affiche "Bonjour [nom]"
+function bonjour(nom) {
+    console.log(`Bonjour ${nom}`);
+}
+
+// j'appel la fonction et j'affiche le résultat
+bonjour("Marcel"); // Il affichera "Bonjour Marcel"
+bonjour("Alice");  // Il affichera "Bonjour Alice"
